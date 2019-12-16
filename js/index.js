72352a1e73788729a475d0c2be7deb6be768c8c7
KolyaShevelinda <kolyashevelinda@gmail.com>
2019-12-16 20:53:02 +0200

    added form update

--- js/index.js
+++ js/index.js
@@ -19,7 +19,7 @@
 // console.log('instance', instance);
 
 import DB from './db.js';
-import { studentsListElement } from './variables.js';
+import { studentsListElement, updateStudentFormElement } from './variables.js';
 import { appendStudent } from './utils.js';
 
 let database = new DB('https://frontend-lectures.firebaseio.com');
@@ -36,3 +36,39 @@ database.getStudents().then(result => {
     console.log('studentList', studentsList);
     studentsList.forEach(student => appendStudent(student, studentsListElement));
 })
+
+studentsListElement.addEventListener('click', event => {
+    let target = event.target;
+
+    if (!target.hasAttribute('data-id')) return;
+
+    event.preventDefault();
+
+    let studentId = target.getAttribute('data-id');
+
+    database.getStudentById(studentId).then(responce => {
+        console.log('responce', responce);
+
+        for (let key in responce) {
+            if (updateStudentFormElement.elements[key]) {
+                updateStudentFormElement.elements[key].value = responce[key];
+            }
+        }
+    });
+});
+
+updateStudentFormElement.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    let data = {};
+    
+    for (let key in this.elements) {
+        if (!this.elements[key].hasAttribute || !this.elements[key].hasAttribute('name')) continue;
+
+        console.log(this.elements[key]);
+        data[this.elements[key].getAttribute('name')] = this.elements[key].value;
+
+        console.log(data);
+    }
+    
+});
